35fe969b738bab86e82ddba68a8c530aaec6f086
ryselis <djryse@gmail.com>
2014-05-24 14:33:36 +0300

    fixed models

--- src/Astropedia/user_management/models.py
+++ src/Astropedia/user_management/models.py
@@ -1,19 +1,20 @@
+# -*- coding: UTF-8 -*-
 from django.db import models
-from django.db.models.fields import CharField, FloatField
+from django.db.models.fields import CharField, FloatField, IntegerField
 from django.db.models.fields.related import ForeignKey
-from django.contrib.auth import models
+from django.contrib.auth import models as auth_models
 
 # Create your models here.
 class ScientistsAsks(models.Model):
-    date = models.DateTimeField(u'Prašymo data', default=timezone.now)
+    date = models.DateTimeField(u'Prašymo data', auto_now_add=True)
     status = CharField(u'Statusas', max_length=1, null=False, blank=False, unique=True)
     user = models.ForeignKey('User', verbose_name=u'Vartotojas')
 
-class User(models.User):
+class User(auth_models.User):
 	is_banned =  models.BooleanField(u'Ar uždraustas', default=False)
 
 class UserSubmittedInfo(models.Model):
 	odjecktId = IntegerField(u'Objekto Numeris')
 	status = CharField(u'Statusas', max_length=1, null=False, blank=False, unique=True)
-	date = models.DateTimeField(u'Prašymo data', default=timezone.now)
+	date = models.DateTimeField(u'Prašymo data',auto_now_add=True)
 	user = models.ForeignKey('User', verbose_name=u'Vartotojas')

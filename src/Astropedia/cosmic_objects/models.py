900dc74653322e97a09eb655bbb354e258dd46ea
ryselis <djryse@gmail.com>
2014-05-24 13:58:29 +0300

    fixed

--- src/Astropedia/cosmic_objects/models.py
+++ src/Astropedia/cosmic_objects/models.py
@@ -5,14 +5,15 @@ from django.db.models.fields.related import ForeignKey
 # Create your models here.
 
 class Constellation(models.Model):
-    name = CharField('Pavadinimas', max_length=32, null=False, blank=False)
-    abbreviation = CharField('Santrumpa', max_length=8, null=False, blank=False,
+    name = CharField(u'Pavadinimas', max_length=32, null=False, blank=False)
+    abbreviation = CharField(u'Santrumpa', max_length=8, null=False, blank=False,
                              unique=True)
     
 class AstronomicalObject(models.Model):
-    constellation = ForeignKey('Constellation', verbose_name='Žvaigždynas')
-    rectascence = FloatField('Rektascencija')
-    declination = FloatField('Deklinacija')
-    distance = FloatField('Atstumas')
-    visible_magnitude = FloatField('Regimasis ryškis')
-    name = CharField('')
+    constellation = ForeignKey(u'Constellation', verbose_name='Žvaigždynas')
+    rectascence = FloatField(u'Rektascencija')
+    declination = FloatField(u'Deklinacija')
+    distance = FloatField(u'Atstumas')
+    visible_magnitude = FloatField(u'Regimasis ryškis')
+    name = CharField(u'Pavadinimas', max_length=32, null=False, blank=True)
+    type = CharField(u'Tipas', max_length=32)
